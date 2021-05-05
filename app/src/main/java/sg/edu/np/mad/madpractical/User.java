ae4c0a8797c43713473af9a5e9ef10f6ec572299
JieShengNP <s10205049@connect.np.edu.sg>
2021-05-05 14:33:19 +0800

    Remove User Class Constructor

--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -38,18 +38,4 @@ public class User {
     public void setFollowed(boolean followed) {
         this.followed = followed;
     }
-
-    public User(String name, String description, int id) {
-        this.name = name;
-        this.description = description;
-        this.id = id;
-        this.followed = false;
-    }
-
-    public User(String name, String description, int id, boolean followed) {
-        this.name = name;
-        this.description = description;
-        this.id = id;
-        this.followed = followed;
-    }
 }
