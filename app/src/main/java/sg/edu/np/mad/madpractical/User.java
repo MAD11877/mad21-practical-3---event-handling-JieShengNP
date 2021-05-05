6a2defc1258a1d336251917aedbbedab67153e16
JieShengNP <s10205049@connect.np.edu.sg>
2021-05-05 14:46:47 +0800

    Change User Class Properties To Public

--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -2,10 +2,10 @@ package sg.edu.np.mad.madpractical;
 
 public class User {
 
-    private String name;
-    private String description;
-    private int id;
-    private boolean followed;
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
 
     public String getName() {
         return name;
